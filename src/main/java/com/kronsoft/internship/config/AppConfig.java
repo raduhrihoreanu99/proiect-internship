fcbabbcab05f4967df9c3a8784d440fe60a26d69
Hrihoreanu, Radu <Radu.Hrihoreanu@noventi.de>
2022-09-01 08:39:06 +0300

    implement form login functionality

--- src/main/java/com/kronsoft/internship/config/AppConfig.java
+++ src/main/java/com/kronsoft/internship/config/AppConfig.java
@@ -1,17 +1,29 @@
 package com.kronsoft.internship.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.hibernate.Session;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.server.Cookie.SameSite;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -40,14 +52,41 @@ public class AppConfig {
 	    return transactionManager;
 	  }
 	  
-	  @Bean
-	  public WebMvcConfigurer corsConfigurer() {
-		  return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-				}
-			};
-	  }
+//	  @Bean
+//	  public WebMvcConfigurer corsConfigurer() {
+//		  return new WebMvcConfigurer() {
+//				@Override
+//				public void addCorsMappings(CorsRegistry registry) {
+//					registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//				}
+//			};
+//	  }
+	  
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilterBean() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+
+		List<String> originsPatterns = new ArrayList<>();
+		originsPatterns.add("http://localhost:4200");
+		config.setAllowedOriginPatterns(originsPatterns);
+
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+	
+//	@Bean
+//	public TomcatContextCustomizer sameSiteCookiesConfig() {
+//		return context -> {
+//			final Rfc6265CookieProcessor cookiePorcessor = new Rfc6265CookieProcessor();
+//			cookiePorcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+//			context.setCookieProcessor(cookiePorcessor);
+//		};
+//	}
 	  
 }
---
+++ src/main/java/com/kronsoft/internship/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.kronsoft.internship.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Bean 
+	public PasswordEncoder passwordEncoder() { 
+	    return new BCryptPasswordEncoder(); 
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/**").authenticated()
+		.and().formLogin().defaultSuccessUrl("/patients")
+		.and().logout().logoutSuccessUrl("/").clearAuthentication(true).deleteCookies("JSESSIONID");
+	}
+	
+	@Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		 auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+    }
+	
+}
